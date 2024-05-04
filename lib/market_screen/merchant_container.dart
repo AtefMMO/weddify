22ef76c8787153abdd966a1f5f1ef6e2ae38cd68
AtefMMO <mohamedatefimb@gmail.com>
2024-05-04 23:09:59 +0300

    category images added also translation

--- lib/market_screen/merchant_container.dart
+++ lib/market_screen/merchant_container.dart
@@ -1,10 +1,17 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:weddify/app_theme/app_theme.dart';
 import 'package:weddify/market_screen/category_screen.dart';
 
 class MerchantContainer extends StatelessWidget {
   String? category;
-  MerchantContainer({required this.category});
+  String? categoryTranslate;
+  String? categoryImage;
+  MerchantContainer(
+      {required this.category,
+      required this.categoryTranslate,
+      required this.categoryImage});
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -15,14 +22,74 @@ class MerchantContainer extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
-          decoration: BoxDecoration(color: Colors.black38),
-          height: MediaQuery.of(context).size.height * 0.1,
-          width: MediaQuery.of(context).size.width * 0.9,
-          child: Center(
-              child: Text(
-            category ?? 'Category Name',
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20,fontFamily: 'RTL-MochaYemen-Sugar'),
-          )),
+          child: Stack(children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                categoryImage!,
+                width: double.infinity,
+                height: 160, // Set the desired height here
+                fit: BoxFit.cover, // Adjust the fit to cover the entire space
+              ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    Text(
+                      categoryTranslate ?? 'Category Name',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        fontFamily: 'RTL-MochaYemen-Sugar',
+                        foreground: Paint()
+                          ..style = PaintingStyle.stroke
+                          ..strokeWidth = 4
+                          ..color = Colors.black,
+                      ),
+                    ),
+                    Text(
+                      categoryTranslate ?? 'Category Name',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        fontFamily: 'RTL-MochaYemen-Sugar',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+               Stack(
+                  children: [
+                    Text(
+                      category ?? 'Category Name',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          fontFamily: 'RTL-MochaYemen-Sugar',
+                        foreground: Paint()
+                          ..style = PaintingStyle.stroke
+                          ..strokeWidth = 4
+                          ..color = Colors.black,),
+                    ),Text(
+                      category ?? 'Category Name',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        fontFamily: 'RTL-MochaYemen-Sugar',
+                       ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ]),
         ),
       ),
     );
